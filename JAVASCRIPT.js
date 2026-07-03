e5d353cb2f0f3c86ab1b623f73b3f03425d30506
aizazahmad736 <aizazexforwardian@gmail.com>
2026-07-03 15:50:11 +0500

    Update web.css and other project files

---
+++ JAVASCRIPT.js
@@ -0,0 +1,127 @@
+// Question 1
+// let age = 20;
+
+// if (age >= 18) {
+//     console.log("You are eligible to vote.");
+// }
+
+
+// question 2:
+// let number = 25;
+// if(number>0){
+//     console.log("number is positive.");
+// }
+
+
+// question 3
+// let marks =100;
+// if (marks == 100){
+//     console.log("the score is perfect");
+// }
+
+// q4
+// let loggedin=true;
+// if(loggedin){
+//     console.log("the user is logged in.");
+// }
+
+
+
+// q5
+// let number=17;
+// if(number%2==0){
+//     console.log("the number is even.")
+// } else{
+//     console.log("the number is odd.")
+// }
+
+
+
+
+// q6
+// let age=16;
+// if(age>=18){
+//     console.log("he can drive the car")
+// }else{
+//     console.log("he cannot drive the car")
+// }
+
+
+
+// q7
+// let number=10;
+// if(number>0){
+//     console.log("the number is positive.")
+// }else{
+//     console.log("the number is negative.")
+// }
+
+
+
+// q8
+// let password="admin123";
+// if(password.length>=8){
+//     console.log("the password is correct.")
+// }else{
+//     console.log("the password is incorrect.")
+// }
+
+
+// q9
+//  let marks=15;
+//  if(marks>=50){
+//      console.log("grade C.")
+//  }else if(marks>=60){
+//      console.log("grade B.")
+//  }else if(marks>=70){
+//      console.log("grade A.")
+//  }else{
+//      console.log(" FAILED.")
+//  }
+
+
+// let a=50;
+// let b=70;
+
+// if(a>b){
+//     console.log("a is greater than b.")
+// }else if(a<b){
+//     console.log("b is greater than a.")
+// }else{
+//     console.log("a is equal to b.")
+// }
+
+
+
+// let day = 3;
+
+// switch (day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("tuesday");
+//         break;
+//     case 3:
+//         console.log("wednesday");
+//         break;
+//     case 4:
+//         console.log("thursday");
+//         break;
+//     case 5:
+//         console.log("friday");
+//         break;
+//     case 6:
+//         console.log("saturday");
+//         break;
+//     case 7:
+//         console.log("sunday");
+//         break;
+//     default:
+//         console.log("invalid day")
+// }
+
+
+
+
+
